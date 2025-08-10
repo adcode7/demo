3464d5e626a3fa067e3ee60e226ff21f6fb4f1d5
Adnansidd07 <124447488+Adnansidd07@users.noreply.github.com>
2025-08-11 00:07:59 +0530

    next day

---
+++ 04.js
@@ -0,0 +1,36 @@
+/*            <<<<<< how does js code execute and Hoistin >>>>>
+
+JS is a synchronies and single thread language 
+
+synchronies means: line by line  execute that code 
+
+
+*when we write any code : execution context make in execution context 
+1.)Memory allocation 
+2.)code execution
+
+
+
+temporal dead zone in (let or const ) it is in  TDZ when ever we can not assign the valtue of that variable
+  
+
+
+when i print any variable before initialization 
+it gives us error i.e : variable is not initialize
+
+
+
+when i print var variable before initialization 
+then it give us "undefined"
+
+why ?
+
+
+Hoistin : (just a concept not implementation) js automatically move the deceleration of variable and function  part move to top of containing scope
+during compilation phase
+
+
+
+
+
+*/
