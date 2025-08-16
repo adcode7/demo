6679609c8bd026b94e33e537c0d17e09028696a3
Adnansidd07 <124447488+Adnansidd07@users.noreply.github.com>
2025-08-16 17:25:47 +0530

    some practice project

---
+++ DOM/04.js
@@ -0,0 +1,11 @@
+// **************  Event Bubbling and capturing ********************
+// What are Event Capturing and Bubbling?
+
+
+
+/*
+Event capturing and bubbling are two phases of event propagation
+in the DOM ,allowing for a structured approach to event handling 
+when dealing with nested elements.
+
+*/
---
+++ bgchanger/script.js
@@ -0,0 +1,10 @@
+//using event bubbling approach
+
+const root=document.getElementById('root');
+
+root.addEventListener('click',(event)=>{
+    if(event.target.tagName=='BUTTON'){
+    document.body.style.backgroundColor=event.target.id;
+    }
+    
+})
